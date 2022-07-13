cc2ceb425fd8551b35ab488bef17a6fb7b16a55a
MEGHWANT SINGH <68533246+Meghwantsingh@users.noreply.github.com>
2022-07-13 23:19:32 +0530

    Add files via upload
    Admin page open when username is admin and password is correct.

---
+++ ONGC-Project/adminhome.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>adminhome</title>
+</head>
+<body>
+
+    <body style="background-color: rgb(0,222,233);">
+    <center>
+    <h1>Adminhome</h1>
+  
+    <br>
+    <div style="background-color: rgb(161, 240, 81); width:500px">
+    <form method="get">
+        <br><br>
+        <br><br>
+        <a  href="registration.html">Registration Work</a> &nbsp; &nbsp;
+        <a  href="">General Work</a>
+        <!-- <input type="submit" value="Registration Work" style="width: 200px"> -->
+        <!-- <input type="submit" value="General Work" style="width: 200px"> -->
+        <br><br>
+    </form>
+</div>
+</center>
+    
+
+</body>
+</html>
